f94e7de45193ebf46c4439e173f3928d6869c613
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-09 14:12:43 +0000

    completed program

--- .ccls-cache/@home@runner@Ch11Q10-compare-strings-pointers/main.c
+++ .ccls-cache/@home@runner@Ch11Q10-compare-strings-pointers/main.c
@@ -1,6 +1,33 @@
+// Program to test if two strings are equal using pointers
+
 #include <stdio.h>
+#include <stdbool.h>
+
+// Declare Functions
+bool areEqual (const char *, const char *);
 
 int main(void) {
-  printf("Hello World\n");
+
+  // Define Strings
+  const char s1[] = "string compare test";
+  const char s2[] = "string";
+
+  printf ("%d\n", areEqual(s1, s2));
+  printf ("%d\n", areEqual(s1, s1));
+  printf ("%d\n", areEqual(s2, "string"));
+  
   return 0;
+}
+
+// Define Functions
+bool areEqual (const char *str1, const char *str2) {
+  bool equal = false;
+
+
+  for (  ; *str1 && *str2; str1++, str2++) {
+  }
+  if (*str1 == *str2)
+    equal = true;
+
+  return equal;
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Programming in C
+## Chapter 11
+### Question 10
+
+This problem is from chapter 11 of the textbook[^1]. This program compares strings using pointers.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 277 : Q10
